1569298a25ff2dc006a29cca7bdccda2dfbbdd05
philipperemy <premy@cogent.co.jp>
2019-03-28 15:31:19 +0900

    update

--- count_reviews.py
+++ count_reviews.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     json_filenames = glob('comments/*.json')
     c = 0
     for json_filename in json_filenames:
-        with open(json_filename, 'r') as r:
+        with open(json_filename, 'r', encoding='utf8') as r:
             c += len(json.load(r))
     print(c)
